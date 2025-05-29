c0bde533c9c9a79fa851437325a1ef3c2b8404c8
mvghasty <fasmagoric.dev@gmail.com>
2025-05-29 04:24:11 -0300

    0.1.4: guides read fix patch

--- lib/mysteries.rb
+++ lib/mysteries.rb
@@ -2,25 +2,25 @@
 module Mysteries
   class JoyfulM
     def self.read
-      exec "cd guides/ && less JOYFUL-MYSTERIES"
+      exec "cd #{__dir__} && cd guides/ && less JOYFUL-MYSTERIES"
     end  
   end
 
   class SorrowfulM
     def self.read
-      exec "cd guides/ && less SORROWFUL-MYSTERIES"
+      exec "cd #{__dir__} && cd guides/ && less SORROWFUL-MYSTERIES"
     end
   end
 
   class GloriousM
     def self.read
-      exec "cd guides/ && less GLORIOUS-MYSTERIES"
+      exec "cd #{__dir__} && cd guides/ && less GLORIOUS-MYSTERIES"
     end
   end
 
   class LuminousM
     def self.read
-      exec "cd guides/ && less LUMINOUS-MYSTERIES"
+      exec "cd #{__dir__} && cd guides/ && less LUMINOUS-MYSTERIES"
     end
   end
 end
